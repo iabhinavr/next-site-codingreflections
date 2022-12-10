6d7d6489c9ca81b1784c160e688a0fcdf2b400d2
Abhinav R <abhinav7r@gmail.com>
2022-12-11 02:16:41 +0530

    added revalidation api

--- next.config.js
+++ next.config.js
@@ -15,6 +15,12 @@ const nextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'http',
+        hostname: 'wordpress.local',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 }
--- pages/index.js
+++ pages/index.js
@@ -61,41 +61,6 @@ export default function Home({ allWpPosts }) {
           ))}
         </ul>
         <div className="py-4">
-          {/* <button className="load-more font-bold block mx-auto px-4 py-2 rounded text-brand-dark bg-brand-skyblue disabled:opacity-75"
-          onClick={async (event) => {
-            let clickedBtn = event.target;
-            clickedBtn.innerHTML = 'Loading more posts...';
-            clickedBtn.disabled = true;
-            let newPosts = {
-              pageInfo: {
-
-              },
-              nodes: []
-            };
-            const morePosts = await getMorePosts(posts.pageInfo.endCursor);
-            newPosts.pageInfo = morePosts.pageInfo;
-            posts.nodes.map((node) => {
-              newPosts.nodes.push(node);
-            });
-            morePosts.nodes.map((node) => {
-              newPosts.nodes.push(node);
-            });
-            setPosts(newPosts);
-
-            if(morePosts.pageInfo.hasNextPage) {
-              clickedBtn.disabled = false;
-              clickedBtn.innerHTML = 'Load more posts';
-            }
-            else {
-              clickedBtn.disabled = true;
-              clickedBtn.innerHTML = 'No more posts to load';
-            }
-
-            
-          }}
-          >
-            Load more posts
-          </button> */}
           <LoadMore posts={posts} onPostsChange={setPosts} />
         </div>
       </section>
